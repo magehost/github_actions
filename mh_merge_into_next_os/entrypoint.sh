030ddb5cc28ef056c040a4f035666e959d179ef1
Jeroen Vermeulen - MageHost <jeroen@magehost.pro>
2021-02-22 16:29:02 +0100

    mh_merge_into_next_os  action

--- mh_merge_into_next_os/entrypoint.sh
+++ mh_merge_into_next_os/entrypoint.sh
@@ -9,16 +9,9 @@ set  -o xtrace  -o errexit  -o nounset  -o pipefail  +o history
 #
 # This script is called from  /srv/magehost/.github/workflows/push.yml
 
-set  -o xtrace  -o errexit  -o nounset  -o pipefail
-
 BRANCH="$( echo "${GITHUB_REF}" | cut -d'/' -f3 )"
-
-if [ "bionic" == "$BRANCH" ]; then
-    # git clone -b focal https://${GITHUB_TOKEN}@github.com/magehost/mhservers.git
-    # git status
-    git status
-    git checkout "origin/$NEXT_BRANCH" -b "$NEXT_BRANCH"
-    git merge bionic && date
-    git status
-fi
+git status
+git checkout --track "origin/$NEXT_BRANCH"
+git merge "$BRANCH"
+git status
 
